869928315d268a3cca2cebb1a5a3f07a60362528
Hajime Terasawa <terako.studio@gmail.com>
2022-06-06 21:41:44 +0900

    style: fix typo

--- pkg/config/config.go
+++ pkg/config/config.go
@@ -6,8 +6,8 @@ import (
 
 // ViperConfig is unmarshaled from viper.Config.
 type ViperConfig struct {
-	TargetPath    string   `mapstructure:"target_path"`
-	ExcludePathes []string `mapstructure:"exclude_pathes"`
-	Plugins       []pluggable.PluginConfig
-	Mode          string
+	TargetPath   string   `mapstructure:"target_path"`
+	ExcludePaths []string `mapstructure:"exclude_paths"`
+	Plugins      []pluggable.PluginConfig
+	Mode         string
 }
