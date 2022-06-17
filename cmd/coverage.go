044bbe168106a2f1323a2f4ff8c65a37e8586742
Hajime Terasawa <terako.studio@gmail.com>
2022-06-17 11:00:02 +0900

    fix: make sure killing plugin child processes by splitting the execute logic into different func

--- cmd/coverage.go
+++ cmd/coverage.go
@@ -1,23 +1,12 @@
 package cmd
 
 import (
+	"fmt"
 	"os"
-	"sync"
 
-	"github.com/hashicorp/go-hclog"
-	"github.com/hashicorp/go-plugin"
 	"github.com/spf13/cobra"
 
-	"github.com/commentcov/commentcov/pkg/common"
-	"github.com/commentcov/commentcov/pkg/filepath"
-	"github.com/commentcov/commentcov/pkg/pluggable"
-	"github.com/commentcov/commentcov/pkg/report"
-	"github.com/commentcov/commentcov/proto"
-)
-
-const (
-	// pluginFileBatch determines how many files each plugin process will process.
-	pluginFileBatch int = 100
+	"github.com/commentcov/commentcov/pkg/execute"
 )
 
 // coverageCmd returns coverage info.
@@ -26,88 +15,11 @@ var coverageCmd = &cobra.Command{
 	Short: "generate coverage reports",
 	Long:  "generate coverage reports",
 	Run: func(cmd *cobra.Command, args []string) {
-		logger := hclog.New(&hclog.LoggerOptions{
-			Output:     hclog.DefaultOutput,
-			Level:      hclog.Trace,
-			Name:       "commentcov",
-			JSONFormat: true,
-		})
-
-		excluded, err := filepath.NewExcludeFileSet(cfg.ExcludePaths)
-		if err != nil {
-			logger.Error(err.Error())
+		if err := execute.Run(cfg); err != nil {
+			_, _ = fmt.Fprintf(os.Stderr, "%v\n", err.Error())
 			os.Exit(1)
 		}
 
-		fileset, err := filepath.Extract(cfg.TargetPath, excluded)
-		if err != nil {
-			logger.Error(err.Error())
-			os.Exit(1)
-		}
-
-		pWG := &sync.WaitGroup{}
-		queue := make(chan common.Pair[[]*proto.CoverageItem, error])
-		for _, ext := range fileset.Extensions() {
-			pc := pluggable.FindPluginConfig(cfg.Plugins, ext)
-			if pc == nil {
-				continue
-			}
-
-			pluginName := pc.Name()
-			logger.Info("Install Plugin", "plugin", pluginName)
-			if err = pc.Install(); err != nil {
-				logger.Error(err.Error())
-				os.Exit(1)
-			}
-
-			cmd := pc.GetExecuteCommand()
-			client := plugin.NewClient(&plugin.ClientConfig{
-				HandshakeConfig: pluggable.PluginHandshakeConfig,
-				VersionedPlugins: map[int]plugin.PluginSet{
-					1: {
-						pluginName: &pluggable.CommentcovPlugin{},
-					},
-				},
-				Cmd: cmd,
-				AllowedProtocols: []plugin.Protocol{
-					plugin.ProtocolGRPC,
-				},
-				Logger: logger,
-			})
-			defer client.Kill()
-
-			p, err := pluggable.GetPluginFromClient(client, pluginName)
-			if err != nil {
-				logger.Error(err.Error())
-				os.Exit(1)
-			}
-
-			batched := common.Batched(fileset.Files(ext), pluginFileBatch)
-			pWG.Add(len(batched))
-
-			for _, files := range batched {
-				go pluggable.Publish(pWG, logger, p, files, excluded, queue)
-			}
-		}
-
-		go func() {
-			pWG.Wait()
-			close(queue)
-		}()
-
-		items, err := pluggable.Consume(logger, queue)
-		if err != nil {
-			logger.Error(err.Error())
-			os.Exit(1)
-		}
-
-		mode, err := report.StringToMode(cfg.Mode)
-		if err != nil {
-			logger.Error(err.Error())
-			os.Exit(1)
-		}
-
-		report.Report(mode, items)
 		os.Exit(0)
 	},
 }
---
+++ pkg/execute/execute.go
@@ -0,0 +1,101 @@
+package execute
+
+import (
+	"fmt"
+	"sync"
+
+	"github.com/hashicorp/go-hclog"
+	"github.com/hashicorp/go-plugin"
+
+	"github.com/commentcov/commentcov/pkg/common"
+	"github.com/commentcov/commentcov/pkg/config"
+	"github.com/commentcov/commentcov/pkg/filepath"
+	"github.com/commentcov/commentcov/pkg/pluggable"
+	"github.com/commentcov/commentcov/pkg/report"
+	"github.com/commentcov/commentcov/proto"
+)
+
+const (
+	// pluginFileBatch determines how many files each plugin process will process.
+	pluginFileBatch int = 100
+)
+
+func Run(cfg config.ViperConfig) error {
+	logger := hclog.New(&hclog.LoggerOptions{
+		Output:     hclog.DefaultOutput,
+		Level:      hclog.Trace,
+		Name:       common.ProjectName,
+		JSONFormat: true,
+	})
+
+	excluded, err := filepath.NewExcludeFileSet(cfg.ExcludePaths)
+	if err != nil {
+		return fmt.Errorf("%w", err)
+	}
+
+	fileset, err := filepath.Extract(cfg.TargetPath, excluded)
+	if err != nil {
+		return fmt.Errorf("%w", err)
+	}
+
+	pWG := &sync.WaitGroup{}
+	queue := make(chan common.Pair[[]*proto.CoverageItem, error])
+	for _, ext := range fileset.Extensions() {
+		pc := pluggable.FindPluginConfig(cfg.Plugins, ext)
+		if pc == nil {
+			continue
+		}
+
+		pluginName := pc.Name()
+		logger.Info("Install Plugin", "plugin", pluginName)
+		if err = pc.Install(); err != nil {
+			return fmt.Errorf("%w", err)
+		}
+
+		cmd := pc.GetExecuteCommand()
+		client := plugin.NewClient(&plugin.ClientConfig{
+			HandshakeConfig: pluggable.PluginHandshakeConfig,
+			VersionedPlugins: map[int]plugin.PluginSet{
+				1: {
+					pluginName: &pluggable.CommentcovPlugin{},
+				},
+			},
+			Cmd: cmd,
+			AllowedProtocols: []plugin.Protocol{
+				plugin.ProtocolGRPC,
+			},
+			Logger: logger,
+		})
+		defer client.Kill()
+
+		p, err := pluggable.GetPluginFromClient(client, pluginName)
+		if err != nil {
+			return fmt.Errorf("%w", err)
+		}
+
+		batched := common.Batched(fileset.Files(ext), pluginFileBatch)
+		pWG.Add(len(batched))
+
+		for _, files := range batched {
+			go pluggable.Publish(pWG, logger, p, files, excluded, queue)
+		}
+	}
+
+	go func() {
+		pWG.Wait()
+		close(queue)
+	}()
+
+	items, err := pluggable.Consume(logger, queue)
+	if err != nil {
+		return fmt.Errorf("%w", err)
+	}
+
+	mode, err := report.StringToMode(cfg.Mode)
+	if err != nil {
+		return fmt.Errorf("%w", err)
+	}
+
+	report.Report(mode, items)
+	return nil
+}
